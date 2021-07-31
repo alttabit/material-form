664e15367b02ec21c39411d3421321feab370532
Mitchell <73150899+alttabit@users.noreply.github.com>
2021-07-31 12:32:28 +1200

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { MaterialUIFormSubmit } from "./components/MaterialUIFormSubmit";
+
+function App() {
+  return (
+    <div className="App">
+      <MaterialUIFormSubmit
+        formName="Sample Form Submit"
+        formDescription="This is sample form using Material UI."
+      />
+    </div>
+  );
+}
+
+export default App;
